111c26fb4fb362b89c06f5cc6ba21731dffc091c
Aidan Mulaokar <aidanmulaokar@Aidans-MacBook-Pro-3.local>
2022-04-20 03:04:43 -0400

    server updates

--- city-rec/src/App.js
+++ city-rec/src/App.js
@@ -4,9 +4,12 @@ import MapChart from './components/MapChart';
 import NavBar from './components/NavBar';
 import { useState } from 'react';
 import { Form } from 'react-bootstrap';
+import React from 'react';
+import {RangeStepInput} from 'react-range-step-input';
 
 function App() {
 
+  //Demographic Information
   const [age, setAge] = useState(0);
   const [gender, setGender] = useState("");
   const [race, setRace] = useState("");
@@ -14,13 +17,49 @@ function App() {
   const [children, setChildren] = useState(0);
   const [education, setEducation] = useState("");
   const [employment, setEmployment] = useState("");
-  const [income, setincome] = useState(0);
+  const [income, setIncome] = useState(0);
+
+  //Personalized Information
+  const [cityPopulation, setCityPopulation] = useState(5);
+  const [cityMedianAge, setCityMedianAge] = useState(5);
+  const [cityPropertyValue, setCityPropertyValue] = useState(5);
+  const [cityHouseholdIncome, setCityHouseholdIncome] = useState(5);
+  const [cityHouseholdOwnership, setCityHouseholdOwnership] = useState(5);
+  const [cityBizTech, setCityBizTech] = useState(5);
+  const [cityUniversities, setCityUniversities] = useState(5);
+  const [cityDiversity, setCityDiversity] = useState(5);
+  const [cityGenderWageGap, setCityGenderWageGap] = useState(5);
+  const [cityAvgWageGap, setCityAvgWageGap] = useState(5);
+  const [cityPublicTransit, setCityPublicTransit] = useState(5);
+  const [cityWalkBike, setCityWalkBike] = useState(5);
+  const [cityWFH, setCityWFH] = useState(5);
+  const [cityExercise, setCityExercise] = useState(5);
+  const [cityCrime, setCityCrime] = useState(5);
+  const [cityPolitical, setCityPolitical] = useState(5);
+  const [userPolitical, setUserPolitical] = useState("");
 
   const [test, setTest] = useState({lon : -74.006, lat: 40.7128});
   const [form, setForm] = useState(0);
   
   const getResults = () => {
     const inputs = {
+      city_pop : cityPopulation,
+      city_median_age: cityMedianAge,
+      city_property_value: cityPropertyValue,
+      city_household_income: cityHouseholdIncome,
+      city_household_ownership: cityHouseholdOwnership,
+      city_biz_tech: cityBizTech,
+      city_universities: cityUniversities,
+      city_diversity: cityDiversity,
+      city_gender_wage_gap: cityGenderWageGap,
+      city_avg_wage_gap: cityAvgWageGap,
+      city_public_transit: cityPublicTransit,
+      city_walk_bike: cityWalkBike,
+      city_wfh: cityWFH,
+      city_exercise: cityExercise,
+      city_crime: cityCrime,
+      city_political: cityPolitical,
+      user_political: userPolitical,
       age : age,
       gender : gender,
       race : race,
@@ -30,7 +69,7 @@ function App() {
       employment: employment,
       income : income
     }
-    setForm(1);
+    setForm(2);
     fetch("/results",
     {
       method: 'POST',
@@ -43,10 +82,14 @@ function App() {
     ).then(
       data => {
         setTest(data)
-        setForm(2);
+        setForm(3);
         console.log(test)
       }
     )
+  }
+
+  const submitFirst = () => {
+    setForm(1)
   }
   return (
     <div className="App">
@@ -59,7 +102,102 @@ function App() {
             </td>
             <td>
               {form === 0 ? 
-              <div>
+                <div>
+                  <div>
+                    <label>How much importance do you place on city population?</label>
+                    <span><RangeStepInput min={0} max ={10} step={1} onChange={(e) => setCityPopulation(e.target.value)}/>{cityPopulation}</span>
+                  </div>
+
+                  <div>
+                    <label>How much importance do you place on city median age?</label>
+                    <span><RangeStepInput min={0} max ={10} step={1} onChange={(e) => setCityMedianAge(e.target.value)}/>{cityMedianAge}</span>
+                  </div>
+
+                  <div>
+                    <label>How much importance do you place on city property value?</label>
+                    <span><RangeStepInput min={0} max ={10} step={1} onChange={(e) => setCityPropertyValue(e.target.value)}/>{cityPropertyValue}</span>
+                  </div>
+
+                  <div>
+                    <label>How much importance do you place on city household income?</label>
+                    <span><RangeStepInput min={0} max ={10} step={1} onChange={(e) => setCityHouseholdIncome(e.target.value)}/>{cityHouseholdIncome}</span>
+                  </div>
+
+                  <div>
+                    <label>How much importance do you place on city household ownership?</label>
+                    <span><RangeStepInput min={0} max ={10} step={1} onChange={(e) => setCityHouseholdOwnership(e.target.value)}/>{cityHouseholdOwnership}</span>
+                  </div>
+
+                  <div>
+                    <label>How much importance do you place on city business and tech jobs?</label>
+                    <span><RangeStepInput min={0} max ={10} step={1} onChange={(e) => setCityBizTech(e.target.value)}/>{cityBizTech}</span>
+                  </div>
+
+                  <div>
+                    <label>How much importance do you place on city universities?</label>
+                    <span><RangeStepInput min={0} max ={10} step={1} onChange={(e) => setCityUniversities(e.target.value)}/>{cityUniversities}</span>
+                  </div>
+
+                  <div>
+                    <label>How much importance do you place on city diversity?</label>
+                    <span><RangeStepInput min={0} max ={10} step={1} onChange={(e) => setCityDiversity(e.target.value)}/>{cityDiversity}</span>
+                  </div>
+
+                  <div>
+                    <label>How much importance do you place on city gender wage gap?</label>
+                    <span><RangeStepInput min={0} max ={10} step={1} onChange={(e) => setCityGenderWageGap(e.target.value)}/>{cityGenderWageGap}</span>
+                  </div>
+
+                  <div>
+                    <label>How much importance do you place on city average wage gap?</label>
+                    <span><RangeStepInput min={0} max ={10} step={1} onChange={(e) => setCityAvgWageGap(e.target.value)}/>{cityAvgWageGap}</span>
+                  </div>
+
+                  <div>
+                    <label>How much importance do you place on city public transit?</label>
+                    <span><RangeStepInput min={0} max ={10} step={1} onChange={(e) => setCityPublicTransit(e.target.value)}/>{cityPublicTransit}</span>
+                  </div>
+
+                  <div>
+                    <label>How much importance do you place on city walking or biking facilities?</label>
+                    <span><RangeStepInput min={0} max ={10} step={1} onChange={(e) => setCityWalkBike(e.target.value)}/>{cityWalkBike}</span>
+                  </div>
+
+                  <div>
+                    <label>How much importance do you place on city work from home facilities?</label>
+                    <span><RangeStepInput min={0} max ={10} step={1} onChange={(e) => setCityWFH(e.target.value)}/>{cityWFH}</span>
+                  </div>
+
+                  <div>
+                    <label>How much importance do you place on city exercise facilities?</label>
+                    <span><RangeStepInput min={0} max ={10} step={1} onChange={(e) => setCityExercise(e.target.value)}/>{cityExercise}</span>
+                  </div>
+
+                  <div>
+                    <label>How much importance do you place on city crime rate?</label>
+                    <span><RangeStepInput min={0} max ={10} step={1} onChange={(e) => setCityCrime(e.target.value)}/>{cityCrime}</span>
+                  </div>
+
+                  <div>
+                    <label>How much importance do you place on city political orientation?</label>
+                    <span><RangeStepInput min={0} max ={10} step={1} onChange={(e) => setCityPolitical(e.target.value)}/>{cityPolitical}</span>
+                  </div>
+
+                  <div>
+                    <label>To which political party do you most closely lean?</label>
+                    <select onChange={(e) => console.log(e.target.value)}>
+                      <option></option>
+                      <option>Democratic</option>
+                      <option>Rebuplican</option>
+                    </select>
+                  </div>
+
+                  <button onClick={() => submitFirst()}>Submit</button>
+                </div>
+
+                : form === 1 ?
+
+                <div>
                 <div>
                   <label>What is your age?</label>
                   <input onChange={(e) => setAge(e.target.value)}/>
@@ -76,7 +214,7 @@ function App() {
 
                 <div>
                   <label>Which of the following best describes your racial identity?</label>
-                  <select>
+                  <select onChange={(e) => setRace(e.target.value)}>
                     <option>American/Alaskan Native</option>
                     <option>Asian</option>
                     <option>Black</option>
@@ -90,7 +228,7 @@ function App() {
 
                 <div>
                   <label>Which of the following best describes your marital status?</label>
-                  <select>
+                  <select onChange={(e) => setMarital(e.target.value)}>
                     <option>Married</option>
                     <option>Unmarried</option>
                   </select>
@@ -98,12 +236,12 @@ function App() {
 
                 <div>
                   <label>How many children do you have?</label>
-                  <input></input>
+                  <input onChange={(e) => setChildren(e.target.value)}></input>
                 </div>
 
                 <div>
                   <label>Which of the following is the highest level of education you have completed?</label>
-                  <select>
+                  <select onChange={(e) => setEducation(e.target.value)}>
                     <option>Some High School</option>
                     <option>High School / GED</option>
                     <option>2-4 Year College Degree</option>
@@ -112,7 +250,7 @@ function App() {
 
                 <div>
                   <label>Which of the following best describes your current employment status?</label>
-                  <select>
+                  <select onChange={(e) => setEmployment(e.target.value)}>
                     <option>Student</option>
                     <option>Employed</option>
                     <option>Self-Employed</option>
@@ -123,7 +261,7 @@ function App() {
                 
                 <div>
                   <label>Which of the following best estimates your personal annual income?</label>
-                  <select>
+                  <select onChange={(e) => setIncome(e.target.value)}>
                     <option>Less than $35,000</option>
                     <option>$35,000 - $75,000</option>
                     <option>Greater than $75,000</option>
@@ -132,12 +270,10 @@ function App() {
 
                 <button onClick={() => getResults()}>Search</button>
               </div>
-                : form === 1 ?
-                  <p>Analyzing...</p>
+                : form === 2 ?
+                <p>Analyzing</p>
                 :
-                <p>
-                  Second Line of Questioning
-                </p>
+                <p>Results</p>
               }
  
               
--- server/server.py
+++ server/server.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
 import json
 import time
+import pandas as pd
+import numpy as np
+from catboost import CatBoostRegressor
 
 
 app = Flask(__name__)
@@ -8,9 +11,56 @@ app = Flask(__name__)
 
 @app.route("/results", methods=['POST'])
 def results():
+    #inputs = request.get_json(force=True)
+    df = pd.read_csv("Final Dataset.csv")
+    cities = pd.read_csv("City_Data_Processed.csv")
+
+    ytrain = df["MENTHLTH"]
+    Xtrain = df.drop("MENTHLTH", axis=1)
+
+    # request is a json object containing information from the UI, as json
     inputs = request.get_json(force=True)
-    time.sleep(10)
     print(inputs)
+    city_pol = inputs["city_political"]
+    if city_pol > 0:
+        city_pol_pref = inputs["user_political"]
+        if city_pol_pref == "Democratic":
+            city_demo = city_pol
+            city_rep = 0
+        else:
+            city_rep = city_pol
+            city_demo = 0
+
+    cat = CatBoostRegressor(loss_function="RMSE",
+                            feature_weights={"Population": inputs["city_pop"],
+                                             "Median Age": inputs["city_median_age"],
+                                             "Property Value": inputs["city_property_value"],
+                                             "Household Income": inputs["city_household_income"],
+                                             "Household Ownership": inputs["city_household_ownership"],
+                                             "Percent Business & Tech Jobs": inputs["city_biz_tech"],
+                                             "Num Universities": inputs["city_universities"],
+                                             "Diversity": inputs["city_diversity"],
+                                             "Gender Wage Gap": inputs["city_gender_wage_gap"],
+                                             "Average Wage": inputs["city_avg_wage_gap"],
+                                             "Take Public Transit": inputs["city_public_transit"],
+                                             "Walks or Bikes": inputs["city_walk_bike"],
+                                             "Work From Home": inputs["city_wfh"],
+                                             "Access To Exercise Opportunities": inputs["city_exercise"],
+                                             "Violent Crime": inputs["city_crime"],
+                                             "Voted Democrat 2016": city_demo,
+                                             "Voted Republican 2016": city_rep})
+    cat.fit(Xtrain, ytrain)
+
+    demographic_features = pd.DataFrame(inputs, columns=['age', 'gender', 'race', 'marital',
+                                                         'children', 'education', 'employment', 'income'])
+    demographic_features['key'] = 1
+    cities['key'] = 1
+    X_pred = demographic_features.merge(cities, on='key').drop("key", 1)
+    y_pred = cat.predict(X_pred)
+    print(y_pred)
+    # time.sleep(10)
+    # print(inputs)
+
     return {"lat": 47.6062, "lon": -122.3321}
 
 
